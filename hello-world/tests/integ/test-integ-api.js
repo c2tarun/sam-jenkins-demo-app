1d96cf7726894a8cb3f9c3028803e2ed6474054b
Tarun <c2tarun@users.noreply.github.com>
2020-11-19 15:05:44 -0800

    integration tests working on local.

---
+++ hello-world/tests/integ/test-integ-api.js
@@ -0,0 +1,36 @@
+const assert = require("chai").assert;
+const AWS = require("aws-sdk");
+const axios = require("axios");
+
+const cloudformation = new AWS.CloudFormation();
+
+// Stack name provided through environment variable.
+const STACK_NAME = process.env["STACK_NAME"];
+// Value declared in Outputs of template.yaml
+const ENDPOINT_KEY = "HelloWorldApi";
+
+describe("GET api test", async () => {
+  let apiResponse;
+  
+  // Fetch API Endpoint from Cloudformation output.
+  before(async () => {
+    const response = await cloudformation
+      .describeStacks({
+        StackName: STACK_NAME,
+      })
+      .promise();
+    const endpointOutput = response.Stacks[0].Outputs.find(
+      (x) => x.OutputKey === ENDPOINT_KEY
+    );
+    const apiEndpoint = endpointOutput.OutputValue;
+    apiResponse = await axios.get(apiEndpoint);
+  });
+
+  it("verifies if response code is 200", async () => {
+    assert.equal(apiResponse.status, 200);
+  });
+
+  it("verifies if response contains my usename", async () => {
+    assert.include(apiResponse.data.message, "Tarun");
+  });
+});
